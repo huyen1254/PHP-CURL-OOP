94d81b3d72e3c04bc870e6ac52906f1e6ca66dfc
huyen125 <53432310+huyen1254@users.noreply.github.com>
2019-12-09 14:19:09 +0700

    taoclassPageDactory

--- Application/Site/PagesFactory.php
+++ Application/Site/PagesFactory.php
@@ -1,31 +1,28 @@
 <?php
+use Site\Functions\MatchesData;
+use Site\PageCrawler\Dantri;
+use Site\PageCrawler\Vietnamnet;
+use Site\PageCrawler\Vnexpress;
 
-// namespace Site;
+class PagesFactory
+{
+    public $html;
 
-// use Site\Functions\MatchesData;
-// use Site\PageCrawler\Dantri;
-// use Site\PageCrawler\Vietnamnet;
-// use Site\PageCrawler\Vnexpress;
-
-// class PagesFactory
-// {
-//     public $html;
-
-//     function makeWebsite(string $param)
-//     {
-//         switch (strtolower($param)) {
-//             case 'vnexpress':
-//                 return new Vnexpress($this->html);
-//                 break;
-//             case 'vietnamnet':
-//                 return new Vietnamnet($this->html);
-//                 break;
-//             case 'dantri':
-//                 return new Dantri($this->html);
-//                 break;
-//             default:
-//                 return null;
-//                 break;
-//         }
-//     }
-// }
+    function makeWebsite(string $param)
+    {
+        switch (strtolower($param)) {
+            case 'vnexpress':
+                return new Vnexpress($this->html);
+                break;
+            case 'vietnamnet':
+                return new Vietnamnet($this->html);
+                break;
+            case 'dantri':
+                return new Dantri($this->html);
+                break;
+            default:
+                return null;
+                break;
+        }
+    }
+}
