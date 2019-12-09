872d1a3ef6e53f49a33c4643deae9b37889bb3cb
huyen125 <53432310+huyen1254@users.noreply.github.com>
2019-12-09 14:27:12 +0700

    updatefactoryController

--- Application/Controllers/FactoryController.php
+++ Application/Controllers/FactoryController.php
@@ -9,21 +9,21 @@ class FactoryController extends Controller
 {
     public function getFactory($dataPage, PagesFactory $page)
     {
-        // $keyPage = array(
-        //     'vnexpress', 'vietnamnet', 'dantri'
-        // );
+        $keyPage = array(
+            'vnexpress', 'vietnamnet', 'dantri'
+        );
        
-        // foreach ($keyPage as $param) {
-        //     // Searches if dataPage  for a match to the regular expression given in param
-        //     if (preg_match("/$param/", $dataPage['host'])) {
-        //         $page->html = $dataPage['html'];
-        //         $website = $page->makeWebsite($param);
-        //         $title = $website->getTitle();
-        //         $date = $website->getDate();
-        //         $content = $website->getContent();
-        //     }
-        // }
-        // echo du lieu
+        foreach ($keyPage as $param) {
+            // Searches if dataPage  for a match to the regular expression given in param
+            if (preg_match("/$param/", $dataPage['host'])) {
+                $page->html = $dataPage['html'];
+                $website = $page->makeWebsite($param);
+                $title = $website->getTitle();
+                $date = $website->getDate();
+                $content = $website->getContent();
+            }
+        }
+        //echo du lieu
         echo '<h2> ' . $title . '</h2> ' . $date  . $content;
 
     }
