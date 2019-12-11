5234ce7f8961fddb4fea5e33e61c8c0d8e62df75
huyen125 <53432310+huyen1254@users.noreply.github.com>
2019-12-11 16:12:28 +0700

    updateregex

---
+++ Factory/MatchesData.php
@@ -0,0 +1,23 @@
+<?php
+class MatchesData
+{
+
+    function matchesDate($regex, $key, $contentHTML)
+    {
+        preg_match($regex, $contentHTML, $date);
+        return $date[$key];
+    }
+
+    function matchesContent($regexParent, $regexChild, $contentHTML)
+    {
+        preg_match_all($regexParent, $contentHTML, $matches, PREG_SET_ORDER, 1);
+
+        preg_match_all($regexChild, $matches[0][1], $content, PREG_SET_ORDER, 1);
+
+        $output = '';
+        foreach ($content as $para) {
+            $output .= $para[1];
+        }
+        return $output;
+    }
+}
